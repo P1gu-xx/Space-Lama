8290f603182a77464c6ef7baa5d384b2bfc69842
da Silva Michael <michaeldasilva96@gmail.com>
2014-08-10 22:29:05 +0200

    Création du projet

---
+++ android/src/ch/p1gu/spacelama/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ch.p1gu.spacelama.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ch.p1gu.spacelama.MyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGame(), config);
+	}
+}
---
+++ desktop/src/ch/p1gu/spacelama/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ch.p1gu.spacelama.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ch.p1gu.spacelama.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
